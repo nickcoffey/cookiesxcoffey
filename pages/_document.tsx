a3896b888417225a341e33647883c8f8ae1f1c31
Nick Coffey <nicholasjcoffey@gmail.com>
2022-10-04 12:26:59 -0500

    prettier update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&display=swap'
+          rel='stylesheet'
         />
       </Head>
       <body>
