9049db574751b5eacdcbcd735940473f6b42f5cf
Nick Coffey <nicholasjcoffey@gmail.com>
2022-10-05 18:49:33 -0500

    change material icons to variable size

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
         {/* Material Icons */}
         <link
           rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap'
         />
       </Head>
       <body className='text-black bg-lightgrey'>
