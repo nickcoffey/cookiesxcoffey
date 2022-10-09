dc9a11fd81c0b4da613393e55d71e1471a555319
Nick Coffey <nicholasjcoffey@gmail.com>
2022-10-09 12:13:46 -0500

    temp about me text

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -3,25 +3,7 @@ import { Section } from '.'
 export const AboutSection = () => {
   return (
     <Section id='about' header='About Me'>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae dolor
-        doloremque perferendis amet mollitia animi, sit dicta aut rem nulla
-        quia, deserunt laborum consequatur, voluptates ipsa. Ullam doloribus
-        error tenetur et, qui eius eos molestias sed optio fugiat quidem
-        blanditiis possimus atque reiciendis suscipit, sapiente aliquid officiis
-        quis. Earum minima vel, adipisci rem officia sed molestiae modi
-        inventore aliquid quod delectus culpa! Iste accusamus delectus dolores
-        ipsa temporibus sed, quia soluta rerum, reiciendis quisquam facilis enim
-        repellendus ab nobis praesentium? Autem saepe, quibusdam deleniti vero
-        soluta repellendus voluptatum laudantium nemo inventore illo distinctio
-        earum voluptatem possimus quos repellat tempora accusamus? Voluptatibus
-        officia deserunt aperiam. Delectus perferendis laudantium quaerat
-        aliquid exercitationem, quam recusandae rerum! Nesciunt labore, odit in
-        obcaecati enim libero ab neque ducimus recusandae, mollitia ex id sequi,
-        velit voluptatem eveniet dolores aut iste architecto distinctio quod
-        similique! Nihil sit consectetur quisquam adipisci laboriosam voluptates
-        odit fugit dolore sapiente officia.
-      </p>
+      <p>Coming soon...</p>
     </Section>
   )
 }
