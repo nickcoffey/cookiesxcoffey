23a951902d6b89cf54ea6f23301c7cde0ee89ecd
Nick Coffey <nicholasjcoffey@gmail.com>
2023-08-15 23:03:52 -0500

    enabled vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
